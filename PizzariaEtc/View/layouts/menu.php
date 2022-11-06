cd23aaca334cad97cdef6856331b4a14a761b2c1
Ronie Peterson <roniepetersondf@gmail.com>
2022-11-06 13:42:31 -0300

    bugfix: links e imagem do menu.

--- PizzariaEtc/View/layouts/menu.php
+++ PizzariaEtc/View/layouts/menu.php
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-expand-lg mb-3" style="background-color: #1e81b0;">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="../Assets/imagens/pizza-logo.png" alt="Logo" width="50" height="30" class="d-inline-block align-text-top">
+      <img src="/Assets/imagens/pizza-logo.png" alt="Logo" width="50" height="30" class="d-inline-block align-text-top">
       <a href="../view" class="btn"><strong>Pizzaria ETC</strong></a>
     </a>
 
@@ -16,7 +16,7 @@
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll " style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link text-white" href="view/usuarios/pedidos/meuspedidos.php">Meus Pedidos</a>
+          <a class="nav-link text-white" href="/view/usuarios/pedidos/meuspedidos.php">Meus Pedidos</a>
         </li>
         <li class="nav-item">
           <a class="nav-link text-white" href="#">Meu Perfil</a>
