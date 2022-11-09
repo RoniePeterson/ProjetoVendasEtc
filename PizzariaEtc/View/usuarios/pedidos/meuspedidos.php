5ae55fe5893fe2141a9bf389614ef5704a420dd3
Ronie Peterson <roniepetersondf@gmail.com>
2022-11-09 19:14:39 -0300

    bugfix: correção nome controle

--- PizzariaEtc/View/usuarios/pedidos/meuspedidos.php
+++ PizzariaEtc/View/usuarios/pedidos/meuspedidos.php
@@ -3,7 +3,7 @@ session_start();
 require_once  "../../../App/Control/verificaLogadoControl.php";
 require_once "../../layouts/header.php";
 require_once  "../../layouts/menu.php";
-require_once "../../../App/Control/listaPedidosControl.php";
+require_once "../../../App/Control/meusPedidosControl.php";
 $qtd = 1;
 ?>
 <section class="container mt-3">
