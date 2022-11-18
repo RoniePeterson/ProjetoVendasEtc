37dd5df8a8846918d070dee40e50f90ad3984a77
Ronie Peterson <roniepetersondf@gmail.com>
2022-11-18 12:19:31 -0300

    jogo da forca e teste

--- python/jogos/forca.py
+++ python/jogos/forca.py
@@ -1,25 +1,43 @@
 # forca.py
 def jogar():
-    print("***************************************************")
-    print("************Bem vindo ao jogo de Forca!************")
-    print("***************************************************")
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-    palavra_secreta = "banana"
-    enforcou = False
-    acertou = False
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_", "_", "_", "_"]
 
-    while(not enforcou and not acertou):
-        chute = input("Qual a letra? ")
-        chute = chute.strip()
-        
-        index =0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a lentra {} na posição {}.".format(letra, index))
-            index = index + 1
-        
-        print("Jogando...")
 
+    erros = 0
+    print(len(palavra_secreta))
+    print(letras_acertadas)
+
+    while(True):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        if (erros == 6):
+            break
+        if ("_" not in letras_acertadas):
+            break
+        print(letras_acertadas)
+
+
+    if("_" not in letras_acertadas):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+    
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
