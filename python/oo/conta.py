f5345123f0b1ac3677cced312846687ffa4c67f6
Ronie Peterson <ronie.peterson@edu.se.df.gov.br>
2022-11-24 11:47:01 -0300

    cap03 - metodos da classe

---
+++ python/oo/conta.py
@@ -0,0 +1,7 @@
+# conta.py
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
