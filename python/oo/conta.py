8ff95afac99a456321390ff1391f7bf06750a000
Ronie Peterson <roniepetersondf@gmail.com>
2022-11-25 11:55:36 -0300

    cap05 - atributos privados e get e setters

--- python/oo/conta.py
+++ python/oo/conta.py
@@ -1,16 +1,32 @@
 # conta.py
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo é {}, do titular: {}".format(self.saldo, self.titular))
+        print("O saldo é {}, do titular: {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+    
+    def transfere(self, destino, valor):
+        self.saca(valor)
+        destino.deposita(valor)
+    
+    def get_saldo(self):
+        return self.__saldo
+      
+    def get_titular(self):
+        return self.__titular
+    
+    def get_limite(self):
+        return self.__limite
+      
+    def set_limite(self, valor):
+      self.__limite =valor
